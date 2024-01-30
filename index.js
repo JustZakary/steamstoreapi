857f130789d94f0d37a360fa974c0e73533e2d81
Zakary Loney <zakaryloney@gmail.com>
2024-01-30 14:14:50 -0500

    removed node_mod

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
-//Created by Zakary Loney (September 2022)
 const axios = require('axios');
-const HTML = require('node-html-parser');
+const {parse} = require('node-html-parser');
 
+function convertObject(input) {
+  const referenceTable = {
+    term: 'term',
+    software_type: 'category1',
+    additional_features: 'category2',
+    multiplayer: 'category3',
+    only_specials: 'specials',
+    hide_free: 'hidef2p',
+    max_price: 'maxprice',
+    controller_support: 'controllersupport',
+    vr_support: 'vrsupport',
+    supported_os: 'os',
+    deck_compatibility: 'deck_compatibility',
+    supported_lang: 'supportedlang',
+    tags: 'tags',
+    count: 'count',
+  };
 
-function search(options, callback) {
-    var url = `https://store.steampowered.com/search/suggest?term=${options.search}&f=${options.type || 'games'}&cc=${options.country || 'US'}&l=${options.language || 'english'}`;
-    axios.get(url).then(resp => {
-        const root = HTML.parse(resp.data);
-        var games = [];
-        root.getElementsByTagName('a').forEach(element => {
-            games.push({
-                name: element.childNodes[0].rawText,
-                link: element.getAttribute('href'),
-                appid: parseFloat(element.getAttribute('data-ds-appid')),
-                image: getImages(element.getAttribute('data-ds-appid')),
-            });
-        });
-        if (games.length > 0) {
-            callback({ games: games, success: true });
-        } else {
-            callback({ message: "No games found with that search.", success: false });
+  let output = {};
+
+  for (let key in input) {
+    if (input[key] !== undefined && referenceTable[key]) {
+      if (typeof input[key] === 'boolean') {
+        if (input[key]) {
+          output[referenceTable[key]] = '1';
         }
-    });
+      } else {
+        output[referenceTable[key]] = input[key];
+      }
+    }
+  }
+
+  if (output.count === undefined) {
+    output.count = '50';
+  }
+
+  return output;
 }
 
-function getImages(appid) {
-    return ({
-        header: `https://cdn.akamai.steamstatic.com/steam/apps/${appid}/header.jpg`,
-        img184x69: `https://cdn.akamai.steamstatic.com/steam/apps/${appid}/capsule_184x69.jpg`,
-        img120x45: `https://cdn.akamai.steamstatic.com/steam/apps/${appid}/capsule_sm_120.jpg`,
-    })
+function searchSteamGames(input) {
+  const query = convertObject(input);
+  var baseUrl = 'https://store.steampowered.com/search/results?snr=1_7_7_230_7&infinite=1';
+  var queryString = Object.keys(query)
+    .map((key) => key + '=' + query[key])
+    .join('&');
+  var url = baseUrl + queryString;
+  console.log(url);
+
+  axios.get(url).then((resp) => {
+    var games = [];
+    //parse html
+    var html = resp.data.results_html;
+    //console.log(html);
+  });
 }
 
-function getGameInfo(options, callback) {
-    var url = `https://store.steampowered.com/api/appdetails?appids=${options.appid}&cc=${options.country || 'US'}&l=${options.language || 'english'}`;
-    console.log(url);
-    axios.get(url).then(resp => {
-        callback(resp.data);
-    });
-
-}
-
-
-
-module.exports = {
-    search: search,
-    getGameInfo: getGameInfo,
-    getImages: getImages,
-}
+searchSteamGames({
+  term: 'action',
+  multiplayer: '2',
+});
